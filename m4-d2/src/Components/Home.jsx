eb2fe697a54c01cde37a0a9089626758edcbe370
Reniejr <reniejr@gmail.com>
2020-11-17 19:27:47 +0100

    updated animations

--- m4-d2/src/Components/Home.jsx
+++ m4-d2/src/Components/Home.jsx
@@ -7,7 +7,11 @@ import scifi from '../data/scifi.json';
 
 class Home extends React.Component{
     render(){
+        console.log(fantasy)
         console.log(history)
+        console.log(horror)
+        console.log(romance)
+        console.log(scifi)
         function split(array, length){
             let chunk = [], i=0, n=array.length
             while(i<n){
@@ -20,19 +24,26 @@ class Home extends React.Component{
         let horror2 = split(horror, 5)
         let romance2 = split(romance, 5)
         let scifi2 = split(scifi, 5)
-        let categories = [fantasy2, history2, horror2, romance2, scifi2]
+        let category = [fantasy, history, horror, romance, scifi]
+        let categories2 = [fantasy2, history2, horror2, romance2, scifi2]
         return <div id="home">
             <div className="slider-prods">
                 <div className="ProdsList">
-                    {categories.map(cat=>{return<div className="prods">
-                        {cat.map(subCat => {return<div className="card">
+                    {categories2.map((cat, index)=>{return<div className="prods" key={index}>
+                        <div className="category-info">
+                            <h4>{cat[0][0].category}</h4>
+                            <p>We have {category[index].length} of {cat[0][0].category} books. Enjoy the collection</p>
+                        </div>
+                        {cat.map((subCat, index) => {return<div className="card" key={index}>
                             {subCat.map((book, index)=>{
                                 return <div className="book" key={index}>
                                 <img src={book.img} alt=""/>
-                            <p className="title-book">{book.title}</p>
-                            <p className="price">{book.price} €</p>
-                            <p className="category">{book.category}</p>
-                            <p className="asin">{book.asin}</p>
+                                    <div className="book-info">
+                                        <p className="title-book">{book.title}</p>
+                                        <p className="price">Price: {book.price} €</p>
+                                        <p className="category">Category: {book.category}</p>
+                                        <p className="asin">Id: {book.asin}</p>
+                                    </div>
                                 </div> 
                             })}
                         </div>
--- m4-d2/src/Components/Welcome.jsx
+++ m4-d2/src/Components/Welcome.jsx
@@ -16,36 +16,31 @@ class Welcome extends Component {
             <div className="slide" key={fantasy[0].asin}>
                  <img src={fantasy[0].img} alt=""/>
                  <div className="title">
-                    <h4>{fantasy[0].title}</h4>
-                    <p>{fantasy[0].category}</p>
+                    <h4>{fantasy[0].category}</h4>
                  </div>
             </div>
             <div className="slide" key={history[0].asin}>
                  <img src={history[0].img} alt=""/>
                  <div className="title">
-                    <h4>{history[0].title}</h4>
-                    <p>{history[0].category}</p>
+                    <h4>{history[0].category}</h4>
                  </div>
             </div>
             <div className="slide" key={horror[0].asin}>
                  <img src={horror[0].img} alt=""/>
                  <div className="title">
-                    <h4>{horror[0].title}</h4>
-                    <p>{horror[0].category}</p>
+                    <h4>{horror[0].category}</h4>
                  </div>
             </div>
             <div className="slide" key={romance[0].asin}>
                  <img src={romance[0].img} alt=""/>
                  <div className="title">
-                    <h4>{romance[0].title}</h4>
-                    <p>{romance[0].category}</p>
+                    <h4>{romance[0].category}</h4>
                  </div>
             </div>
             <div className="slide" key={scifi[0].asin}>
                  <img src={scifi[0].img} alt=""/>
                  <div className="title">
-                    <h4>{scifi[0].title}</h4>
-                    <p>{scifi[0].category}</p>
+                    <h4>{scifi[0].category}</h4>
                  </div>
             </div>
         </div>
