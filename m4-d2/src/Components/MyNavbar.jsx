4a829686e87d32b7f928e5d5db26a9f5b21165e0
Reniejr <reniejr@gmail.com>
2020-11-17 14:44:19 +0100

    navbar toggable

---
+++ m4-d2/src/Components/MyNavbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class MyNavbar extends React.Component {
+    state = {style:{marginTop : '0px'}}
+    togglerClick = ()=>{
+        if(this.state.style.marginTop === '0px'){
+            this.setState({style:{marginTop:'-48px'}})
+        }
+        else{this.setState({style:{marginTop:'0px'}})}
+    }
+    render() {
+        
+        return <nav style={this.state.style}>
+                <div className="toggler" onClick={this.togglerClick}></div>
+                <div className="logo">
+                    <img src={this.props.image} alt=""/>
+                    <div className="userName">
+                        {this.props.userName}
+                    </div>
+                </div>
+                <ul id="nav-list">
+                    <li className="nav-item">
+                        <ion-icon name="home-outline"></ion-icon>
+                        <a href="">{this.props.nav1}</a>
+                    </li>
+                    <li className="nav-item">
+                        <ion-icon name="image-outline"></ion-icon>
+                        <a href="">{this.props.nav2}</a>
+                    </li>
+                    <li className="nav-item">
+                        <ion-icon name="person-outline"></ion-icon>
+                        <a href="">{this.props.nav3}</a>
+                    </li>
+                </ul>
+            </nav>
+    }
+}
+//  ({image, userName, nav1, nav2, nav3})=>
+//     <nav>
+//         <div className="toggler"></div>
+//         <div className="logo">
+//             <img src={image} alt=""/>
+//             <div className="userName">
+//                 {userName}
+//             </div>
+//         </div>
+//         <ul id="nav-list">
+//             <li className="nav-item">
+//                 <ion-icon name="home-outline"></ion-icon>
+//                 <a href="">{nav1}</a>
+//             </li>
+//             <li className="nav-item">
+//                 <ion-icon name="image-outline"></ion-icon>
+//                 <a href="">{nav2}</a>
+//             </li>
+//             <li className="nav-item">
+//                 <ion-icon name="person-outline"></ion-icon>
+//                 <a href="">{nav3}</a>
+//             </li>
+//         </ul>
+//     </nav>
+
+
+export default MyNavbar
--- m4-d2/src/index.js
+++ m4-d2/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import './Components/MyNavbar.css'
 import reportWebVitals from './reportWebVitals';
+import MyNavbar from './Components/MyNavbar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyNavbar image='' userName='RenieJr' nav1='Home' nav2='Gallery' nav3='About'></MyNavbar>
   </React.StrictMode>,
   document.getElementById('root')
 );
